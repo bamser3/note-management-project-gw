ae536e415b0ab7aa6fb30fbb1a43af45d9e8aa7f
bamser <111535201+bamser3@users.noreply.github.com>
2026-02-25 15:22:09 +0300

    storage_class implemented (#6)
    * auth_manager class implemented
    
    * storage_class implementation

---
+++ storage/storage_class.py
@@ -0,0 +1,22 @@
+import json
+import os
+
+class Storage:
+    
+    def __init__(self, filepath: str = "users.json"):
+        self.filepath = filepath
+    
+    def load_users(self) -> dict:
+        if not os.path.exists(self.filepath):
+            return {}
+        
+        with open(self.filepath, "r") as file:
+            try:
+                return json.load(file)
+            except json.JSONDecodeError: # for file corruption
+                return {}
+    
+    def save_users(self, users: dict) -> None:
+        with open(self.filepath, "w") as file:
+            json.dump(users, file, indent= 4)
+        pass
